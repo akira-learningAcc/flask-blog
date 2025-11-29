659f159f67b389b18e0545a4d855a03d5551cb6f
akira-learningAcc <gudafugu2bg@gmail.com>
2025-11-29 14:05:58 +0900

    Add PostgreSQL config and integrate SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+DB_INFO = {
+       'user': 'postgres',
+       'password': 'password',
+       'host': 'localhost',
+       'port': '5432',
+       'database': 'postgres'
+}
+
+db = SQLAlchemy()
+SQLALCHEMY_DATABASE_URI = (
+    "postgresql+psycopg://{user}:{password}@{host}:{port}/{database}"
+).format(**DB_INFO)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+db.init_app(app)
 
 @app.route("/<int:number>")
 def hello_world(number):
