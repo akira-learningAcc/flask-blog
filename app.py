06e17a0fd91ae1dd29b771e8c605e315c95017ac
akira-learningAcc <gudafugu2bg@gmail.com>
2025-11-29 15:16:30 +0900

    Add SQLAlchemy setup, PostgreSQL connection and Post model

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from dotenv import load_dotenv
 load_dotenv()
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import pytz
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -20,6 +22,14 @@ SQLALCHEMY_DATABASE_URI = (
 
 app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 db.init_app(app)
+
+
+class Post(db.Model):
+       id = db.Column(db.Integer, primary_key=True)
+       title = db.Column(db.String(255), nullable=False)
+       body = db.Column(db.Text, nullable=False)
+       tokyo_timezone = pytz.timezone('Asia/Tokyo')
+       created_at = db.Column(db.DateTime,nullable=False,default=datetime.now(tokyo_timezone))
 
 @app.route("/<int:number>")
 def hello_world(number):
